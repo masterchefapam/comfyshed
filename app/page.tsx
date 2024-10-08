735036f622a454fe91b7852bc81ec9f5319a2dd6
apam <masterchefapam@gmail.com>
2024-10-08 11:30:11 +0800

    started adding shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <Button>Hello</Button>
-   
+  
+ 
+     <Button>Hello</Button>
+ 
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className='w-full border-b'>
+            <h1>Hello Fro Navbar</h1>
+        </nav>
+    </div>
+    
+  )
+}
+
+export default Navbar
